144cae74f248eb99ba2505c836d58656c9101d89
kareemka <92390737+kareemka@users.noreply.github.com>
2026-04-24 16:38:02 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
-  const origins = configService.get<string>('CORS_ORIGINS', 'http://localhost:3000,http://localhost:3001').split(',');
 
   app.enableCors({
-    origin: origins,
+    origin: ['https://admin.shadowsilhouette.com', 'https://shadowsilhouette.com', 'https://api.shadowsilhouette.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  const port = configService.get<number>('PORT', 3000);
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
